c7650d762ccd761bb3750c0f450bab246f6539f4
x1nni <x1nni@x1nni.xyz>
2022-12-14 10:18:46 -0600

    complete orderFinalizer and optimize loops

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 name = 'Mr. Potter'
 email = 'apotter@wsd1.org'
 teaID = [0,1,2,3,4,5,6,7]
-teas = ["earlgrey","orangepekoe","peppermint","calmomile","englishbreakfast","chai","green","oolong"]
+teas = ["Earl Grey","Orange Pekoe","Peppermint","Calmomile","English Breakfast","Chai","Green","Oolong"]
+total = 0
 #make the tea names proper english for use in orderFinalizer
 isInStock = [True,True,False,True,True,True,False,True]
 
@@ -9,7 +10,13 @@ isInStock = [True,True,False,True,True,True,False,True]
 
 def main():
     welcomeScreen(name)
-    orderFinalizer(teaPicker())
+    confirmOrder(orderFinalizer(teaPicker()))
+
+def getTotal(item):
+    global total
+    total = total + item
+    return total
+
 
 
 
@@ -18,19 +25,58 @@ def welcomeScreen(name):
     input("Press enter to begin.")
 
 def teaPicker():
-    print("What kind of tea would you like? (input the number)")
-    getTea = int(input("0. Earl Grey \n1. Orange Pekoe \n2. Peppermint \n3.Calmomile \n4.English Breakfast \n5. Chai \n6. Green \n7. Oolong\n"))
-    if getTea <= 7:
-        if isInStock[getTea] == True:
-            return getTea
+    while True:
+        print("What kind of tea would you like? (input the number)")
+        getTea = int(input("0. Earl Grey \n1. Orange Pekoe \n2. Peppermint \n3.Calmomile \n4.English Breakfast \n5. Chai \n6. Green \n7. Oolong\n"))
+        if getTea <= 7:
+            if isInStock[getTea] == True:
+                return getTea
+            else:
+                print("We're sorry, that tea is out of stock! Please check again later, or select a different tea.")
         else:
-            print("We're sorry, that tea is out of stock! Please check again later, or select a different tea.")
-            teaPicker()
-    else:
-        print("Please input the number beside the tea.")
-        teaPicker()
+            print("Please input the number beside the tea.")
 
 def orderFinalizer(tea):
-    print("You selected: "+ teas[tea])
+    while True:
+        print("You selected: "+ teas[tea])
+        getBags = int(input("How many bags would you like? \n0. 24 bags ($16) \n1. 72 bags ($25) \n2. 120 bags ($32)"))
+        if getBags == 0:
+            bagCost = 16
+            break
+        elif getBags == 1:
+            bagCost = 25
+            break
+        elif getBags == 2:
+            bagCost = 32
+            break
+        else:
+            print("Invalid option.")
+    print("Your new total is $"+str(getTotal(bagCost)))
+    while True:
+        getShip = int(input("When would you like it? \n0. Standard (4-10 days) Free \n1. Express (2-4 days) +$10 \n2. Overnight (next day) +$20 \n"))
+        if getShip == 0:
+            shipCost = 0
+            break
+        elif getShip == 1:
+            shipCost = 10
+            break
+        elif getShip == 2:
+            shipCost = 20
+            break
+        else:
+            print("Invalid option.")
+    print("Your new total is $"+str(getTotal(shipCost)))
+    taxAndService = total * 0.14
+    print("+12% GST and PST, +2% Service fee")
+    grandTotal = round(getTotal(taxAndService),2)
+    print("Your grand total is $"+str(grandTotal))
+    return [getTea,getBags,getShip,grandTotal]
+
+def confirmOrder(details):
+    #print details of order using return of orderFinalizer
+    isConfirmed = input("Place your order? (y/n) \n")
+    if isConfirmed == 'y':
+
+
 
 main()
